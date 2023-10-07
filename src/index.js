b93eac5eacec802c13bfc915b2aee6aedf1db107
Ladislav Dono <97441178+donnlaa@users.noreply.github.com>
2023-10-07 22:08:43 +0200

    fixing form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, get } from "firebase/database";
+import { getDatabase, ref, push, set, get } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,34 +21,46 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Reference to the "university" node in the database
-const universitiesRef = ref(db, 'university');
+// json z formulara
+$(document).ready(function () {
+  // Function to save form data as JSON
+  function saveFormDataAsJSON() {
+    // Capture form input values
+    var university = document.getElementById("universitySelect").value;
+    var major = document.getElementById("majorSelect").value;
+    var employment = document.getElementById("employmentSelect").value;
+    var price = document.getElementById("price").value;
+    var rating = document.querySelector(".rating input:checked").value;
 
-// Reference to the <select> element in your HTML
-const universitySelect = document.getElementById('universitySelect');
+    // Create JSON object
+    // Create JSON object
+    var jsonData = {
+      "name": university,
+      "odbor": major,
+      "work": employment,
+      "money": price,
+      "rating": rating // Fix the typo here
+    };
 
-// Retrieve the university data and populate the <select>
-get(universitiesRef)
-  .then((snapshot) => {
-    if (snapshot.exists()) {
-      const universitiesData = snapshot.val();
-      for (const universityId in universitiesData) {
-        if (universitiesData.hasOwnProperty(universityId)) {
-          const university = universitiesData[universityId];
-          const universityName = university.name;
-          const option = document.createElement('option');
-          option.value = universityId; // Set the value to the university_id
-          option.textContent = universityName;
-          universitySelect.appendChild(option);
-        }
+    // You can now use the jsonData as needed, e.g., send it to a server or save it locally
+    console.log(jsonData);
+
+    // Push the data to a specific location with a unique 'university_id'
+    var universitiesRef = ref(db, "university"); // Replace "university" with your desired database path
+
+    // Generate a new 'university_id' using Firebase's push method
+    var newUniversityRef = push(universitiesRef);
+
+    // Set the data under the new 'university_id'
+    set(newUniversityRef, jsonData, function (error) {
+      if (error) {
+        console.error("Data could not be saved." + error);
+      } else {
+        console.log("Data saved successfully.");
       }
-    } else {
-      console.log("University data does not exist.");
-    }
-  })
-  .catch((error) => {
-    console.error("Error getting university data:", error);
-  });
+    });
+  }
 
-  
-  
+  // Add click event listener to the "Pridať" button
+  document.getElementById("saveButton").addEventListener("click", saveFormDataAsJSON);
+});
