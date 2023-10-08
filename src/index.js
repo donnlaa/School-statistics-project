6edb21a6eac5008dfbe01f213aa180060e539001
Ladislav Dono <97441178+donnlaa@users.noreply.github.com>
2023-10-08 16:45:06 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, set, onValue } from "firebase/database";
+import * as $ from 'jquery';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
